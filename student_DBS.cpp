0410dfa64f169f2942080cef9448f3a682eff156
Bharath Kumar S P <86399096+BharathKumar-3@users.noreply.github.com>
2023-02-06 21:49:34 +0530

    Update student_DBS.cpp

--- student_DBS.cpp
+++ student_DBS.cpp
@@ -1,309 +1,290 @@
-// Include all the necessary libraries.  
-#include <fstream>  
-#include <iostream>  
-#include <stdio.h>  
-#include <string.h>  
-using namespace std;  
-int main()  
-{  
-    // Considering the max length of data entered (name) to  
-    // be 15.  
-    char data[15];  
-    int n = 0, option = 0, count_n = 0;  
-    // This is the initial mark alloted to a subject.  
-    string empty = "00";  
-    string proctor = "";  
-    // Name of the file in which DB is stored.  
-    ifstream f("Example.txt");  
-    string line;  
-  
-    // The following for loop counts the total number of  
-    // lines in the file.  
-    for (int i = 0; std::getline(f, line); ++i)  
-    {  
-        count_n++;  
-    }  
-  
-    while (option != 6)  
-    {  
-        // This prints out all the available options in the  
-        // DB  
-        cout << "\nAvailable operations: \n1. Add New "  
-                "Students\n2."  
-             << "Student Login\n3. Faculty Login\n4. "  
-                "Proctor Login\n5. Admin View\n"  
-             << "6. Exit\nEnter option: ";  
-        cin >> option;  
-  
-        if (option == 1)  
-        {  
-            cout << "Enter the number of students: ";  
-            cin >> n;  
-  
-            count_n = count_n + n;  
-  
-            for (int i = 0; i < n; i++)  
-            {  
-                ofstream outfile;  
-                outfile.open("Example.txt", ios::app);  
-                // The entire data of a single student is  
-                // stored line-by-line.  
-                cout << "Enter your registration number: ";  
-                cin >> data;  
-                outfile << data << "\t";  
-  
-                cout << "Enter your name: ";  
-                cin >> data;  
-                int len = strlen(data);  
-  
-                while (len < 15)  
-                {  
-                    data[len] = ' ';  
-                    len = len + 1;  
-                }  
-                outfile << data << "\t";  
-                // Inserting empty data initially into the  
-                // file  
-                outfile << empty << "\t";  
-                outfile << empty << "\t";  
-  
-                cout << "Enter your proctor ID: ";  
-                cin >> proctor;  
-  
-                outfile << proctor << endl;  
-            }  
-        }  
-  
-        else if (option == 2)  
-        {  
-            char regno[9];  
-            cout << "Enter your registration number: ";  
-            cin >> regno;  
-            ifstream infile;  
-            int check = 0;  
-            infile.open("Example.txt", ios::in);  
-  
-            // This loop prints out the data according to  
-            // the registration number specified.  
-            while (infile >> data)  
-            {  
-                if (strcmp(data, regno) == 0)  
-                {  
-                    cout  
-                        << "\nRegistration Number: " << data  
-                        << endl;  
-                    infile >> data;  
-                    cout << "Name: " << data << endl;  
-  
-                    infile >> data;  
-                    cout << "CSE1001 mark: " << data  
-                         << endl;  
-  
-                    infile >> data;  
-                    cout << "CSE1002 mark: " << data  
-                         << endl;  
-  
-                    infile >> data;  
-                    cout << "Proctor ID: " << data << endl;  
-  
-                    infile.close();  
-                    check = 1;  
-                }  
-            }  
-  
-            if (check == 0)  
-            {  
-                cout << "No such registration number found!"  
-                     << endl;  
-            }  
-        }  
-  
-        // This loop is used to view and add marks to the  
-        // database of a student.  
-        else if (option == 3)  
-        {  
-            char subcode[7];  
-            cout << "Enter your subject code: ";  
-            cin >> subcode;  
-            string code1 = "CSE1001", code2 = "CSE1002",  
-                   mark = "";  
-            ifstream infile;  
-            int check = 0;  
-  
-            cout << "\nAvailable operations: \n1. Add data "  
-                    "about marks\n"  
-                 << "2. View data\nEnter option: ";  
-            cin >> option;  
-  
-            if (option == 1)  
-            {  
-                cout  
-                    << "Warning! You would need to add mark"  
-                    << "details for all the students!"  
-                    << endl;  
-                for (int i = 0; i < count_n; i++)  
-                {  
-                    fstream file("Example.txt");  
-  
-                    // The seek in file has been done  
-                    // according to the length  
-                    // of the data being inserted. It needs  
-                    // to adjusted accordingly for different  
-                    // lengths of data.  
-  
-                    if (strcmp(subcode, code1.c_str()) == 0)  
-                    {  
-                        file.seekp(26 + 37 * i,  
-                                   std::ios_base::beg);  
-                        cout << "Enter the mark of student#"  
-                             << (i + 1) << " : ";  
-                        cin >> mark;  
-                        file.write(mark.c_str(), 2);  
-                    }  
-  
-                    if (strcmp(subcode, code2.c_str()) == 0)  
-                    {  
-                        file.seekp(29 + 37 * i,  
-                                   std::ios_base::beg);  
-                        cout << "Enter the mark of student#"  
-                             << (i + 1) << " : ";  
-                        cin >> mark;  
-                        file.write(mark.c_str(), 2);  
-                    }  
-                }  
-            }  
-  
-            // This loop is used to view marks of a student.  
-            // The extra infile commands have been used to  
-            // get a specific mark only since the data has  
-            // been separated by a tabspace.  
-  
-            else if (option == 2)  
-            {  
-                infile.open("Example.txt", ios::in);  
-                if (strcmp(subcode, code1.c_str()) == 0)  
-                {  
-                    cout << "Registration number - Marks\n"  
-                         << endl;  
-                    while (infile >> data)  
-                    {  
-                        cout << data;  
-                        infile >> data;  
-                        infile >> data;  
-                        cout << " - " << data << endl;  
-                        infile >> data;  
-                        infile >> data;  
-                        check = 1;  
-                    }  
-                }  
-  
-                infile.close();  
-                infile.open("Example.txt", ios::in);  
-  
-                if (strcmp(subcode, code2.c_str()) == 0)  
-                {  
-                    cout << "Registration number - Marks\n"  
-                         << endl;  
-                    while (infile >> data)  
-                    {  
-                        cout << data;  
-                        infile >> data;  
-                        infile >> data;  
-                        infile >> data;  
-                        cout << " - " << data << endl;  
-                        infile >> data;  
-                        check = 1;  
-                    }  
-                }  
-            }  
-  
-            infile.close();  
-  
-            if (check == 0)  
-            {  
-                cout << "No such subject code found!"  
-                     << endl;  
-            }  
-        }  
-  
-        // This loop displays all the details of students  
-        // under the same proctor ID.  
-  
-        else if (option == 4)  
-        {  
-            char procid[7];  
-            cout << "Enter your proctor ID: ";  
-            cin >> procid;  
-            int check = 0;  
-            char temp1[100], temp2[100], temp3[100];  
-            char temp4[100], id[100];  
-            ifstream infile;  
-            infile.open("Example.txt", ios::in);  
-  
-            while (infile >> temp1)  
-            {  
-                infile >> temp2;  
-                infile >> temp3;  
-                infile >> temp4;  
-                infile >> id;  
-  
-                if (strcmp(id, procid) == 0)  
-                {  
-                    cout << "\nRegistration Number: "  
-                         << temp1 << endl;  
-                    cout << "Name: " << temp2 << endl;  
-                    cout << "CSE1001 Mark: " << temp3  
-                         << endl;  
-                    cout << "CSE1002 Mark: " << temp4  
-                         << endl;  
-                    check = 1;  
-                }  
-            }  
-  
-            if (check == 0)  
-            {  
-                cout << "No such proctor ID found!" << endl;  
-            }  
-        }  
-  
-        // This loop acts as an admin view to see all the  
-        // data in the file.  
-  
-        else if (option == 5)  
-        {  
-            char password[25];  
-            cout << "Enter the admin password: ";  
-            cin >> password;  
-  
-            // This variable value can be changed according  
-            // to your requirement of the administrator  
-            // password.  
-  
-            string admin_pass = "admin";  
-  
-            if (strcmp(password, admin_pass.c_str()) == 0)  
-            {  
-                cout << "Reg No.   "  
-                        "\tName\tCSE1001\tCSE1002\tProctor "  
-                        "ID"  
-                     << endl;  
-                ifstream infile;  
-                infile.open("Example.txt", ios::in);  
-                char data[20];  
-  
-                while (infile >> data)  
-                {  
-                    cout << data << "\t";  
-                    infile >> data;  
-                    cout << data << "\t";  
-                    infile >> data;  
-                    cout << data << "\t";  
-                    infile >> data;  
-                    cout << data << "\t";  
-                    infile >> data;  
-                    cout << data << endl;  
-                }  
-            }  
-        }  
-    }  
-} 
+#include<string>
+#include<iostream>
+using namespace std;
+struct student {
+    string first_name;
+    string last_name;
+    int roll_number;
+    double cgpa;
+    int course_id[10];
+} students[110];
+
+int number_of_students = 0;
+
+void addStudentDetails()
+{
+ 
+    cout<<"Enter the new student details\n";
+    
+    cout<<"Enter the Roll Number of the student\n";
+    int roll_no;
+    cin>>roll_no;
+    
+    for(int i = 0; i < number_of_students; i++)
+    {
+       if(students[i].roll_number == roll_no)
+       {
+          cout << " Student with the given roll number already exists in the database\n";
+          return;
+       }
+    }
+    
+    cout<<"Enter the first name of the student\n";
+    cin>>students[number_of_students].first_name;
+    
+    cout<<"Enter the last name of the student\n";
+    cin>>students[number_of_students].last_name;
+    
+    students[number_of_students].roll_number = roll_no;
+    
+    cout<<"Enter the CGPA of the student\n";
+    cin>>students[number_of_students].cgpa;
+    
+    cout<<"Enter the course ID of each course in which the student is enrolled in\n";
+    for (int i = 0; i < 5; i++) {
+        cin>>students[number_of_students].course_id[i];
+    }
+    
+    number_of_students++;
+}
+void findStudentByRollNumber()
+{
+    int roll_no;
+    cout<<"Enter the Roll Number of the student\n";
+    cin>>roll_no;
+    
+    bool found = false;
+    
+    for (int i = 0; i < number_of_students; i++) {
+        if (roll_no == students[i].roll_number) {
+            found = true;
+            
+            cout<<"The Students Details are:\n";
+            
+            cout<<"The First name is " << students[i].first_name << "\n";
+            cout<<"The Last name is " << students[i].last_name << "\n";
+            cout<<"The CGPA is " << students[i].cgpa << "\n";
+            cout<<"The course ID of each course\n";
+        
+            for (int j = 0; j < 5; j++) {
+                cout << students[i].course_id[j] << " ";
+            }
+            cout<<"\n";
+            break;
+      }
+    }
+    
+    if(!found)
+       cout<<"no such student with the given roll number\n";
+}
+void findStudentByFirstName()
+{
+    string firstName;
+    cout<<"Enter the first name of the student\n";
+    cin>>firstName;
+    
+    bool found = false;
+    
+    for (int i = 0; i < number_of_students; i++) {
+        if (firstName == students[i].first_name) {
+            found = true;
+            
+            cout<<"The Students Details are\n";
+            
+            cout<<"The First name is " << students[i].first_name << "\n";
+            cout<<"The Last name is " << students[i].last_name << "\n";
+            cout<<"The CGPA is " << students[i].cgpa << "\n";
+            cout<<"The course ID of each course\n";
+        
+            for (int j = 0; j < 5; j++) {
+                cout << students[i].course_id[j] << " ";
+            }
+            
+            cout<<"\n";
+      }
+    }
+    
+    if(!found)
+       cout<<"no such student with the given first name\n";
+}
+void findStudentByCourseId()
+{
+    int id;
+    cout<<"Enter the course id\n";
+    cin>>id;
+    
+    bool found = false;
+    
+    for (int i = 0; i < number_of_students; i++) {
+      
+      for(int j = 0; j < 5; j++){
+      
+         if(id == students[i].course_id[j]){
+            
+            found=true;
+            cout<<"The Students Details are\n";
+            
+            cout<<"The First name is " << students[i].first_name << "\n";
+            cout<<"The Last name is " << students[i].last_name << "\n";
+            cout<<"The CGPA is " << students[i].cgpa << "\n";
+            cout<<"\n";
+         }
+      }      
+        
+    }
+    
+    if(!found)
+       cout<<"no such students with the given course id\n";  
+}
+    
+void findTotalStudents()
+{
+    cout<<"The total number Students are "<< number_of_students <<"\n";
+    cout<<"You can have maximum of 110 students in the database\n";
+}
+void deleteStudentByRollNumber()
+{
+    int roll_no;
+    cout<<"Enter the Roll Number that you want to delete\n";
+    cin>>roll_no;
+    
+    bool found=false;
+    for (int i = 0; i < number_of_students; i++) {
+        if (roll_no == students[i].roll_number) {
+            found = true;
+            for (int j = i; j < 109; j++)
+                students[j] = students[j + 1];
+            number_of_students--;
+        }
+    }
+    
+    if(found){
+       cout<<"The Roll Number is removed Successfully\n";
+    }
+    else{
+       cout<<"Roll number not found in the database\n";
+    }
+    
+}
+void updateStudentDetails()
+{
+ 
+    cout<<"Enter the roll number whose details you want to update\n";
+    int roll_no;
+    cin>>roll_no;
+    
+    bool found=false;
+    for (int i = 0; i < number_of_students; i++) {
+        if (students[i].roll_number == roll_no) {
+        
+            found=true;
+            cout<< "1. update first name\n"
+                   "2. update last name\n"
+                   "3. update roll number.\n"
+                   "4. update CGPA\n"
+                   "5. update courses\n";
+                   
+            int choice;
+            cin>>choice;
+            
+            switch(choice) {
+            case 1:
+                cout<<"Enter the new first name\n";
+                cin>>students[i].first_name;
+                break;
+                
+            case 2:
+                cout<<"Enter the new last name\n";
+                cin>>students[i].last_name;
+                break;
+                
+            case 3:
+                cout<<"Enter the new roll number\n";
+                cin>>students[i].roll_number;
+                break;
+                
+            case 4:
+                cout<<"Enter the new CGPA\n";
+                cin>>students[i].cgpa;
+                break;
+                
+            case 5:
+                cout<<"Enter the new courses \n";
+                cin>>students[i].course_id[0]>>
+                    students[i].course_id[1]>> students[i].course_id[2]>>
+                    students[i].course_id[3]>>students[i].course_id[4];
+                break;
+
+        }
+     }
+   }  
+   
+   if(found){
+     cout<<"Details updated successfully.\n";
+   }
+   else{
+     cout<<"Student not found in the database.\n";
+   }
+}
+int main()
+{
+    int choice;
+    while (true) {
+        cout<<"Choices of the tasks that you want to perform\n";
+        
+        cout<<"1.Add new Student to the database\n";
+        
+        cout<<"2. Search Student by Roll Number\n";
+               
+        cout<<"3. Search Student by First Name\n";
+               
+        cout<<"4. Search Student by Course Id\n";
+               
+        cout<<"5. Count Total number of Students\n";
+               
+        cout<<"6. Delete the Student by Roll Number\n";
+               
+        cout<<"7. Update Student Details by Roll Number\n";
+               
+        cout<<"8. Exit the program\n";
+        
+        cout<<"Enter your choice\n";
+        cin>>choice;
+        
+        switch (choice) {
+        case 1:
+            addStudentDetails();
+            break;
+            
+        case 2:
+            findStudentByRollNumber();
+            break;
+            
+        case 3:
+            findStudentByFirstName();
+            break;
+            
+        case 4:
+            findStudentByCourseId();
+            break;
+            
+        case 5:
+            findTotalStudents();
+            break;
+            
+        case 6:
+            deleteStudentByRollNumber();
+            break;
+            
+        case 7:
+            updateStudentDetails();
+            break;
+            
+        case 8:
+            exit(0);
+            break;
+        }
+    }
+    
+    return 0;
+}
